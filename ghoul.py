41021447d664b430f589ff97026c6a03dc519b12
Teapaty <73647296+ZewsHack@users.noreply.github.com>
2021-11-09 11:04:22 +0200

    I'm a ghoul

--- ghoul.py
+++ ghoul.py
@@ -1,1 +1,1 @@
-print(''.join([f"I'm a ghoul 1000 - {i + 7} = {1000 - (i + 7)}\n\n" for i in range(993)]))
+print("I'm a ghoul\n" + ''.join([f"1000 - {i + 7} = {1000 - (i + 7)}\n\n" for i in range(1000-7)]))
